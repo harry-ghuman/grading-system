2a7834008211767e35ad7c3e5a6a028c1d48a00f
Harpuneet Ghuman <harpuneetghuman@hotmail.com>
2016-07-23 19:25:03 -0400

    gs-commit

---
+++ 3_columns.php
@@ -0,0 +1,49 @@
+
+<div id="main" >
+
+
+<div class="col-md-3" style="margin-top: -60px;" id="left">
+
+    <footer id="icon">
+        <div class="icon-ribbon">
+            <span>About Department</span>
+        </div>
+
+    </footer>
+    <ul style="margin-top: -30px;"><p style="text-align: justify;color:#000000;">
+                The department of Computer Engineering and Technology is established in the year 2015.
+                It is located in Maharaja Ranjit Singh Bhawan .The foundation stone of Maharaja Ranjit Singh bhawan was laid by Dr.Inder Kumar Gujral on Oct 16,1997.
+                The department offers professional courses such as B.Tech & M.Tech.
+          </p></ul>
+    </div>
+
+
+
+    <div class="col-md-3" style="margin-top: -60px;" id="right">
+        <footer id="icon">
+            <div class="icon-ribbon">
+                <span>News</span>
+            </div>
+
+        </footer>
+
+        <ul style="margin-top: -30px;"><p><?php include "news.php"?></p></ul>
+    </div>
+
+        <div class="col-md-3" style="margin-top: -60px;" id="center">
+            <footer id="icon">
+                <div class="icon-ribbon">
+                    <span>HOD's Message</span>
+                </div>
+
+            </footer>
+
+            <ul style="margin-top: -30px;"><p style="text-align: justify;color:#000000;">
+
+                    With a glorious history of 44 years, Guru Nanak Dev University has scaled new heights in academics, sports and cultural activities. The University has carved a niche for itself on the academic map of India and has now become an excellent centre of learning for students from rural, urban and border areas.
+                <br>Dr. Sandeep Sharma
+                </p>
+            </ul>
+        </div>
+
+</div>
